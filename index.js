c7f7a600531d7443ffc9f5c426e68b209aba8d53
Nevine Achola <nevine.achola@student.moringaschool.com>
2023-03-18 17:44:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
+let divide = () => 2000 / 100
 let num1 = 2000
 let num2 = 100
 
 let divide = () => num1 / num2;
-
-
+//
+let square = () => 2 * 2
 
 let square = (num) => num * num;
-let add = (nam1, nam2 ) => nam1 + nam2;
-
-
-
+//
+let add = ( ) => 3 + 4
+let add = (num1, num2 ) => num1 + num2;
